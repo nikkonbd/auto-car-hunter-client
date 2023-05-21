1763730cf8f70045586245b365038d835704d39d
nikkonbd <nikkon1998@gmail.com>
2023-05-21 20:33:36 +0600

    post data added

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -13,6 +13,13 @@ const firebaseConfig = {
     appId: "1:464130520936:web:179fa160d3e13f2cb288dd"
 };
 
+// apiKey: import.meta.env.VITE_apiKey,
+// authDomain: import.meta.env.VITE_authDomain,
+// projectId: import.meta.env.VITE_projectId,
+// storageBucket: import.meta.env.VITE_storageBucket,
+// messagingSenderId: import.meta.env.VITE_messagingSenderId,
+// appId: import.meta.env.VITE_appId
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
